41fbf74b1fb67d9b411e9aa6042ec3f648b2686b
Erdem AY <eirdemmoon@gmail.com>
2023-11-14 23:34:21 +0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,27 +8,28 @@ import ToastProvider from '@/utils/toast.provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: '2XE-Commerce',
+  title: 'The Address of Easy Shopping',
   description: 'The Address of Easy Shopping',
 }
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <StoreInitializer>
-        <ToastProvider>
-          <div
-            className="flex flex-col min-h-screen bg-black">
-            <Navbar />
-            <div className="flex-1 flex flex-col ">
-              {children}
+          <ToastProvider>
+            <div className="flex flex-col min-h-screen bg-black">
+              <Navbar />
+              <div className="flex-1 flex flex-col">
+                {children}
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
           </ToastProvider>
         </StoreInitializer>
       </body>
     </html>
   )
 }
+
